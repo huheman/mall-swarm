d1ccf40d06e0327f885cdff570f96686ed9007b6
876651731@qq.com <876651731@qq.com>
2024-09-21 14:35:32 +0800

    去掉mongoDB

--- mall-portal/src/main/java/com/macro/mall/portal/service/impl/MemberAttentionServiceImpl.java
+++ mall-portal/src/main/java/com/macro/mall/portal/service/impl/MemberAttentionServiceImpl.java
@@ -2,7 +2,6 @@ package com.macro.mall.portal.service.impl;
 
 import com.macro.mall.model.UmsMember;
 import com.macro.mall.portal.domain.MemberBrandAttention;
-import com.macro.mall.portal.repository.MemberBrandAttentionRepository;
 import com.macro.mall.portal.service.MemberAttentionService;
 import com.macro.mall.portal.service.UmsMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * 会员关注Service实现类
@@ -21,48 +19,30 @@ import java.util.List;
 @Service
 public class MemberAttentionServiceImpl implements MemberAttentionService {
     @Autowired
-    private MemberBrandAttentionRepository memberBrandAttentionRepository;
-    @Autowired
     private UmsMemberService memberService;
 
     @Override
     public int add(MemberBrandAttention memberBrandAttention) {
         int count = 0;
-        UmsMember member = memberService.getCurrentMember();
-        memberBrandAttention.setMemberId(member.getId());
-        memberBrandAttention.setMemberNickname(member.getNickname());
-        memberBrandAttention.setMemberIcon(member.getIcon());
-        memberBrandAttention.setCreateTime(new Date());
-        MemberBrandAttention findAttention = memberBrandAttentionRepository.findByMemberIdAndBrandId(memberBrandAttention.getMemberId(), memberBrandAttention.getBrandId());
-        if (findAttention == null) {
-            memberBrandAttentionRepository.save(memberBrandAttention);
-            count = 1;
-        }
         return count;
     }
 
     @Override
     public int delete(Long brandId) {
-        UmsMember member = memberService.getCurrentMember();
-        return memberBrandAttentionRepository.deleteByMemberIdAndBrandId(member.getId(),brandId);
+        return 0;
     }
 
     @Override
     public Page<MemberBrandAttention> list(Integer pageNum, Integer pageSize) {
-        UmsMember member = memberService.getCurrentMember();
-        Pageable pageable = PageRequest.of(pageNum-1,pageSize);
-        return memberBrandAttentionRepository.findByMemberId(member.getId(),pageable);
+        return Page.empty();
     }
 
     @Override
     public MemberBrandAttention detail(Long brandId) {
-        UmsMember member = memberService.getCurrentMember();
-        return memberBrandAttentionRepository.findByMemberIdAndBrandId(member.getId(), brandId);
+        return null;
     }
 
     @Override
     public void clear() {
-        UmsMember member = memberService.getCurrentMember();
-        memberBrandAttentionRepository.deleteAllByMemberId(member.getId());
     }
 }
