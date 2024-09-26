1e7cdfdf9e00bb551e17a238865f5910dc37828f
876651731@qq.com <876651731@qq.com>
2024-09-26 15:47:53 +0800

    请先登录

--- mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
+++ mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
@@ -8,7 +8,7 @@ public enum ResultCode implements IErrorCode {
     SUCCESS(200, "操作成功"),
     FAILED(500, "操作失败"),
     VALIDATE_FAILED(404, "参数检验失败"),
-    UNAUTHORIZED(401, "暂未登录或token已经过期"),
+    UNAUTHORIZED(401, "请先登录"),
     FORBIDDEN(403, "没有相关权限");
     private long code;
     private String message;
