85aaa938407607a5efb78bbddf42847771316bfb
876651731@qq.com <876651731@qq.com>
2024-10-11 15:26:03 +0800

    加上sourceType

--- mall-portal/src/main/java/com/macro/mall/portal/service/impl/OmsPortalOrderServiceImpl.java
+++ mall-portal/src/main/java/com/macro/mall/portal/service/impl/OmsPortalOrderServiceImpl.java
@@ -217,7 +217,7 @@ public class OmsPortalOrderServiceImpl implements OmsPortalOrderService {
         //支付方式：0->未支付；1->支付宝；2->微信
         order.setPayType(0);
         //订单来源：0->PC订单；1->app订单
-        order.setSourceType(order.getSourceType() == null ? 1 : order.getSourceType());
+        order.setSourceType(orderParam.getSourceType() == null ? 1 : orderParam.getSourceType());
         //订单状态：0->待付款；1->待发货；2->已发货；3->已完成；4->已关闭；5->无效订单
         order.setStatus(0);
         //订单类型：0->正常订单；1->秒杀订单
