09c3d9b2f2f64dea321461d0dfa481885c4ae961
876651731@qq.com <876651731@qq.com>
2024-10-14 07:53:48 +0800

    提高isolation

--- mall-portal/src/main/java/com/macro/mall/portal/service/DirectChargeService.java
+++ mall-portal/src/main/java/com/macro/mall/portal/service/DirectChargeService.java
@@ -1,13 +1,14 @@
 package com.macro.mall.portal.service;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface DirectChargeService {
 
     void directCharge(String orderSN);
 
-    @Transactional
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     void chargeSuccess(JSONObject callback) throws Exception;
 
     String chargeStatus(String orderSN);
