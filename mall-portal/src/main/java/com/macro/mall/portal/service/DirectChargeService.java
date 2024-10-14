3f05c03398ea38d1b03af6fbb75d77b0ce41957d
876651731@qq.com <876651731@qq.com>
2024-10-14 08:13:28 +0800

    提高isolation

--- mall-portal/src/main/java/com/macro/mall/portal/service/DirectChargeService.java
+++ mall-portal/src/main/java/com/macro/mall/portal/service/DirectChargeService.java
@@ -8,7 +8,7 @@ public interface DirectChargeService {
 
     void directCharge(String orderSN);
 
-    @Transactional(isolation = Isolation.READ_COMMITTED)
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     void chargeSuccess(JSONObject callback) throws Exception;
 
     String chargeStatus(String orderSN);
